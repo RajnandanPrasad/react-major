89b2c6c03711cd70d8bb7140946429187017a766
Rajnandan Prasad <rajnandanprasad2004@gmail.com>
2024-07-28 15:46:26 +0530

    first commit

---
+++ gfg.java
@@ -0,0 +1,20 @@
+ 
+// Java program to create a user-defined
+// package and function to print
+// a message for the users
+package example;
+ 
+// Class
+public class gfg{
+ 
+    public void show()
+    {
+        System.out.println("Hello geeks!! How are you?");
+    }
+ 
+    public static void main(String args[])
+    {
+        gfg obj = new gfg();
+        obj.show();
+    }
+}
---
+++ hero.java
@@ -0,0 +1,10 @@
+
+import example.gfg;
+ 
+public class hero{
+    public static void main(String args[])
+    {
+        gfg obj = new gfg();
+        obj.show();
+    }
+}
